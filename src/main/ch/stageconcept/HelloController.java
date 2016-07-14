d7e2cb5303bbca66d225b54de620c956efee9ed4
OLDU <oldu@stageconcept.ch>
2016-07-14 15:55:09 +0200

    Initial commit

---
+++ src/main/ch/stageconcept/HelloController.java
@@ -0,0 +1,19 @@
+package ch.stageconcept;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloController{
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String printHello(ModelMap model) {
+        System.out.println("Hi!!");
+        model.addAttribute("message", "Hello Spring MVC Framework! Jaaaa!!!!");
+        return "hello";
+    }
+
+}
